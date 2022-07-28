73ecc5851f9f012c8fccf30c043ccab4ac489ae6
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-07-28 14:48:51 +0900

    4

--- asdq.py
+++ asdq.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import os
+import random
+os.system('color ' +random.choice(['a', 'b', 'c', 'd'])+ " & cls & title  Pictsence Spammer")
 def pict():
     choice = input("Insert 1:" )
     if choice =="1":
@@ -17,4 +20,7 @@ def pict():
                 mail.send_keys("Nagato")
                 element = chrome.find_element_by_xpath ('//*[@id="chatSubmitButton"]')
                 element.submit()
+                exit = input(f'エンターキーを押してください: ')
+                exit = clear()
+                exit = pict()
 pict()
