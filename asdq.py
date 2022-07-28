5dcf4dc9b27d005bbf23911d34d7ca7028bb8d61
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-07-28 14:33:39 +0900

    Update asdq.py

--- asdq.py
+++ asdq.py
@@ -2,12 +2,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-choice = input("Insert 1 and press enter to start generating discord account!:" )
+choice = input("Insert 1:" )
 if choice =="1":
     web="https://pictsense.com/#!/1658918791374_56"
-    chrome = webdriver.Chrome(executable_path='C:\\Users\\tamat\\3D Objects\\自作\\accountgen\\chromedriver.exe')
+    chrome = webdriver.Chrome(executable_path='Your Path')
     chrome.get(web)
-    choicee = input("Insert 1 and press enter to start generating discord account!:" )
+    choicee = input("入室でいたら1をおす" )
     if choicee =="1":
         ka = int(input("何回送りますか？"))
         for i in range(ka):
